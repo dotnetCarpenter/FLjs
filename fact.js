931aa126fa8b0d69fb2a54e7b9e2cb4f4bbd33be
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-05-12 08:16:06 +0200

    factorial without explicit recursiveness in definition

--- fact.js
+++ fact.js
@@ -6,7 +6,10 @@ let l = console.log
 l( `def fact ${trigram} ~0 -> ~1;id * (fact ${middledot} sub1)` )
 l( "fact:5" )
 
-let fact = 
+// main
+let fact = compose(mul, intsto)
+let result = fact(5)
+l( result )
 
 function compose (...f) {
   return (...args) => {
@@ -14,6 +17,17 @@ function compose (...f) {
   }
 }
 
-function construction (...f) {
-  return (...args) => f.map(f => f(...args))
+function mul (...numbers) {
+  if (numbers.length === 0) return 1
+
+  return numbers.reduce((accu, n) => accu * n)
+}
+
+function intsto (to) {
+  let ret = []
+  
+  while (to > 0) 
+    ret.unshift(to--)
+  
+  return ret
 }
