4c9444432aad6c01447262c0c1e9c7823f4f834d
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-05-12 13:21:20 +0200

    simplified "id" function

--- explicitFact.js
+++ explicitFact.js
@@ -12,12 +12,10 @@ l( "fact:5 -> 120" )
 // let _fact
 // let fact = y => y === 0 ? [1] : _fact(y)
 // _fact = compose(mul, compose(fact, sub1))
-// process.exit(0)
 
 let _fact
-let id = y => y === 0 && 1
-let fact = x => id(x) || mul(x, _fact(x))
-_fact = compose(fact, sub1) // fact:5 -> fact:4
+let fact = x => x === 0 ? [1] : mul(x, _fact(x))
+_fact = compose(fact, sub1) // fact:5 -> fact:4 -> etc.
 
 let result = fact(5)
 l( ...result )
@@ -31,10 +29,6 @@ function compose (...f) {
 function construction (...f) {
   return (...args) => f.map(f => f(...args))
 }
-
-// function id (f) {
-//   return (...args) => f(...args)
-// }
 
 function mul (...numbers) {
   if (numbers.length === 0) return [1]
